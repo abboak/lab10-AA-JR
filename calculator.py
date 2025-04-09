4aff3221100c131035581f1a4e009e2a6e4dc12c
abboak <alkaelania@ufl.edu>
2025-04-09 17:26:37 -0400

    hhhhhjihio

--- calculator.py
+++ calculator.py
@@ -1,13 +1,8 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-
-
 import math
+
+# Repository: https://github.com/abboak/lab10-AA-JR
+# Partner 1: Jack Reinhardt
+# Partner 2: Abdulrahman Alkaelani
 
 def add(a, b):
     return a + b
@@ -20,16 +15,17 @@ def mul(a, b):
 
 def div(a, b):
     if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
+        raise ZeroDivisionError("Division by zero is not allowed")
     return a / b
 
 def log(a, b):
     if a <= 0 or a == 1:
-        raise ValueError("Invalid base for logarithm")
+        raise ValueError("Invalid base for logarithm: base must be > 0 and not equal to 1")
     if b <= 0:
         raise ValueError("Logarithm argument must be positive")
     return math.log(b, a)
 
 def exp(a, b):
     return a ** b
+
 
