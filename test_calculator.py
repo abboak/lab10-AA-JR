9cccc98658e8e639489058bc45a5cbe1c50ecb12
abboak <alkaelania@ufl.edu>
2025-04-10 16:10:30 -0400

    hhhhhjihio

--- test_calculator.py
+++ test_calculator.py
@@ -13,9 +13,19 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(sub(10, 5), 5)
-        self.assertEqual(sub(5, 10), -5)
-        self.assertEqual(sub(-1, -1), 0)
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(5, 10), -5)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(-2, 5), -10)
+        self.assertEqual(mul(0, 100), 0)
+
+    def test_divide(self):
+        self.assertAlmostEqual(div(10, 2), 5.0)
+        self.assertAlmostEqual(div(3, 2), 1.5)
+        self.assertAlmostEqual(div(-6, 3), -2.0)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
@@ -25,13 +35,20 @@ class TestCalculator(unittest.TestCase):
         self.assertAlmostEqual(log(2, 8), 3, places=5)
         self.assertAlmostEqual(log(10, 100), 2, places=5)
 
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(2, -10)
+        with self.assertRaises(ValueError):
+            logarithm(2, 0)
+
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            log(1, 10)
+            logarithm(1, 10)
         with self.assertRaises(ValueError):
-            log(-2, 10)
+            logarithm(-2, 10)
 
-    def test_square_root(self):
+    def test_sqrt(self):
         self.assertAlmostEqual(square_root(4), 2.0, places=5)
         self.assertAlmostEqual(square_root(9), 3.0, places=5)
         with self.assertRaises(ValueError):
